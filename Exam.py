56f66aa047193766c2cd4afaa66c9c694844e9bd
whaamkabaam <hi@whaamkabaam.com>
2025-05-05 11:52:05 +0200

    painful task 3 advancements

--- Exam.py
+++ Exam.py
@@ -54,6 +54,8 @@ X Ensure to account for the situation that in either of the two hints there coul
 - Allow the player to play at least 3 games with different words.
 - Allow the player to exit the game any time - e.g., by entering the word ‘exit’.'''
 
+import os
+
 def welcome():
 
     print('''Hi! Welcome to Wordle, your python nightmare of a game.
@@ -65,58 +67,80 @@ def welcome():
         print("hE wAsN't ReAdY.\nProgram terminated.")
         TODO how to termiante? // UPDATE: imported something random but didnt work'''
 
-def game_manager():
+def game_manager(round_counter): # manages if the player wants to play another round
 
     player_is_choosing = True # boolean to enter infinite loop until player has chosen
-    continue_input = input("Thank you for playing.")
+    round_counter += 1
+    word_final = ""
+
+    # declaring which word will be used to play with / to be guessed
 
     while player_is_choosing:
-        print("Do you wish to continue playing? (y/n) ")
-        if continue_input.lower() == "y":
-            continue_input
-            player_is_choosing = False
 
-        if continue_input.lower() == "n":
-            continue_input
+        if round_counter == 1:
+            word_final = "apple"
+            guessing_main(word_final)
+
+        if round_counter == 2:
+            word_final = "chair"
+
+        elif round_counter == 3:
+            word_final = "table"
+
+        elif round_counter > 3:  # if its more than three rounds it quits
+            print("Unfortunately you have reached the daily limit of games. Come back tomorrow!")
+            exit()
+
+        print("Thank you for playing.")
+        continue_input = input("Do you wish to continue playing? (y/n) ")
+        if continue_input.lower() == "y":
             player_is_choosing = False
+            guessing_main(word_final)
+
+        elif continue_input.lower() == "n":
+            player_is_choosing = False
+            print("Still, thank you for playing.")
 
         else:
-            print("Invalid input, please try again.")
+            print("Invalid input, DWDAWDplease try again.")
 
-def success(guess_counter): # success function
+    return round_counter
 
-    print("OMG! You guessed first try. So legendary.") if guess_counter < 2 else print(f"Success! You guessed {final_word_1} in {guess_counter} attempts.")
+def success(guess_counter, to_be_guessed_word): # success function
 
-def player_sucks(): # loss function
+    print("OMG! You guessed first try. So legendary.") if guess_counter < 2 else print(f"Success! You guessed {to_be_guessed_word} in {guess_counter} attempts.")
+    game_manager()
+
+def player_sucks(to_be_guessed_word): # loss function
 
     print("whomp whomp, you lost.")
-    print(f"The correct word would have been {final_word_1}.")
+    print(f"The correct word would have been {to_be_guessed_word}.")
 
-def guessing_main():
+def guessing_main(word_final):
 
+    to_be_guessed_word = word_final
     is_guessing = True # condition to enter infinite loop until player wins or runs out of guesses
     guess_counter = 0
     guesses_left = 6
 
     while is_guessing:
-
         try:
             user_word_guess = (input(str("What word is your guess? "))).lower() # makes sure to disregard caps
             if len(user_word_guess) != 5: # invalid input checker
                 print("This word isn't 5 characters long, please try again.")
-            elif user_word_guess == final_word_1: # correct guess
+            elif user_word_guess == to_be_guessed_word: # correct guess
                 is_guessing = False
                 guess_counter += 1
-                success(guess_counter) # calls success function
+                success(guess_counter, to_be_guessed_word) # calls success function
             elif len(user_word_guess) == 5: # correct input but invalid guess
                 print("Nice guess...")
                 guess_counter += 1
                 guesses_left -= 1
                 print(f"You have {guesses_left} guesses left.")
-                word_compare(final_word_1, user_word_guess) # calls comparing function
+                word_compare(to_be_guessed_word, user_word_guess) # calls comparing function
                 if guesses_left < 1: # is called when player has put correct input but has run out of guesses
                     is_guessing = False
-                    player_sucks() # legendarily named loosing function is called
+                    player_sucks(to_be_guessed_word) # legendarily named loosing function is called
             else:
                 print("You reached unknown land. How did you get here?") # honestly no clue how anyone would end up here
 
@@ -124,9 +148,9 @@ def guessing_main():
             print(E)
             print("The dev f*cked up, please try again.")
 
-def word_compare(final_world, user_word_guess): # word comparing function
+def word_compare(to_be_guessed_word, user_word_guess): # word comparing function
 
-    split_final_word = list(final_world)
+    split_final_word = list(to_be_guessed_word)
     split_guessed_word_raw = list(user_word_guess)
     split_guessed_word = list(dict.fromkeys(user_word_guess)) # this is to remove duplicate letters
     # if we dont do this the print of the letter being in the word loops that amount of times
@@ -137,7 +161,6 @@ def word_compare(final_world, user_word_guess): # word comparing function
 
         if letter in split_final_word:
             letter_matches += letter
-            print(letter_matches)
 
     if letter_matches: # if there are any matching letters this gets executed and informs the player
 
@@ -150,15 +173,13 @@ def word_compare(final_world, user_word_guess): # word comparing function
 
         if split_guessed_word_raw[index] == split_final_word[index]:
             letter_position_matches += matched_letters
-            print(letter_position_matches)
 
     if letter_position_matches: # if there are any matching letters + positions this gets executed and informs the player
-        print(f"So you have done nice work! And even better: {", ".join(letter_position_matches)} is a position match.") \
-            if len(letter_position_matches) < 2 else print(f"Nice! Even better: {", ".join(letter_position_matches)} are a position match.")
+        print(f"So you're getting there! And even better: {", ".join(letter_position_matches)} is a position match.") \
+            if len(letter_position_matches) < 2 else print(f"Even better: {", ".join(letter_position_matches)} are a position match.")
         # same thing here with better syntax
 
 
-final_word_1 = "apple"
 welcome()
-guessing_main()
-game_manager()
+round_counter = 0
+game_manager(round_counter)
